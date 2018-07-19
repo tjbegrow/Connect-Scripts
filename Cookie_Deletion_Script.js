7af3b2a77808b2ab3564f016ea4af8580c91bbec
tjbegrow <41201449+tjbegrow@users.noreply.github.com>
2018-07-18 22:20:47 -0500

    Successfully deleted all cookies except for http only

--- Cookie_Deletion_Script.js
+++ Cookie_Deletion_Script.js
@@ -2,5 +2,10 @@ var cookieArray = document.cookie.split(";");
     for (var i = 0; i < cookieArray.length; i++) {
         var index = cookieArray[i].indexOf("=")
         var str = [cookieArray[i].substring(0, index), cookieArray[i].substring(index)]
-        document.cookie = str[0] + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/; domain=.pb.com;";
-}
+        document.cookie = str[0] + "=; expires=Wed, 01 Jan 1969 00:00:01 GMT; path=/; domain=.pitneybowes.com;";
+        document.cookie = str[0] + "=; expires=Wed, 01 Jan 1969 00:00:01 GMT; path=/; domain=sendpro.pitneybowes.com;";
+        document.cookie = str[0] + "=; expires=Wed, 01 Jan 1969 00:00:01 GMT; path=/; domain=.sendpro.pitneybowes.com;";
+        document.cookie = str[0] + "=; expires=Wed, 01 Jan 1969 00:00:01 GMT; path=/api/foundation/v1/; domain=.sendpro.pitneybowes.com;";
+        document.cookie = str[0] + "=; expires=Wed, 01 Jan 1969 00:00:01 GMT; path=/;";            
+    }
+//One is unable to delete http only cookies because of security reasons, so this might not completely help us
